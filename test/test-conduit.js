2c94736374e0e8ea3752a641aa1ab46cc3d42fff
Antonio Lain <antlai@cafjs.com>
2014-06-14 22:22:41 -0700

    Update drone config

---
+++ test/test-conduit.js
@@ -0,0 +1,7 @@
+var conduit = require('../index');
+
+exports['create'] = function (test) {
+    var c = conduit.newInstance(['foo','bar']);
+    test.ok(c, 'Cannot create conduit');
+    test.done();
+};
