7cb33d86c609b3873d4e11f74c90890ca16ad6fa
Antonio Lain <antlai@cafjs.com>
2014-06-15 14:37:49 -0700

    Test drone CI 4

--- test/test-conduit.js
+++ test/test-conduit.js
@@ -2,6 +2,6 @@ var conduit = require('../index');
 
 exports['create'] = function (test) {
     var c = conduit.newInstance(['foo','bar']);
-    test.ok(c, 'Cannot create conduit. ');
+    test.ok(c, 'Cannot create conduit.');
     test.done();
 };
